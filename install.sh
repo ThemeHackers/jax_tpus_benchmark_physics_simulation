59b84a6514273db56f38ef1f84f93a1047b86b60
_tthemzdl5678 <107047730+ThemeHackers@users.noreply.github.com>
2025-10-22 23:39:21 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pip install --upgrade pip
+pip install torch==2.1.0 torch_xla==2.1.0 -f https://storage.googleapis.com/pytorch-tpu-releases/wheels/tpuvm/torch_xla-2.1.html
+pip install "transformers<5.8"
+pip install jax>=0.6.0 flax orbax-checkpoint clu tensorflow-datasets tensorflow-metadata protobuf<4
+pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install tensorflow tensorflow-metadata psutil rich
+pip check
+
+git clone https://github.com/ThemeHackers/tpu
+cd tpu
+python3 tpus-benchmark_v3.py
