df75ad9dd5032b3042b267412063f25214f0e714
_tthemzdl5678 <107047730+ThemeHackers@users.noreply.github.com>
2025-10-23 13:18:24 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ pip install torch==2.1.0 torch_xla==2.1.0 -f https://storage.googleapis.com/pyto
 pip install "transformers<5.8"
 pip install jax>=0.6.0 flax orbax-checkpoint clu tensorflow-datasets tensorflow-metadata protobuf<4
 pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-pip install psutil rich
+pip install psutil rich matplotlib pandas
 
 
 pip check
+
